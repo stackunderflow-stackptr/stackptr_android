e1c9a888a391c69b60f31d783e12bf1e48a9c475
Geordie Millar <gm@stackunderflow.com>
2014-03-19 23:13:10 +1030

    gps support

--- src/com/stackunderflow/ops/StackOps.java
+++ src/com/stackunderflow/ops/StackOps.java
@@ -1,6 +1,5 @@
 package com.stackunderflow.ops;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStream;
@@ -13,14 +12,19 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.view.Menu;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class StackOps extends Activity {
 
@@ -29,6 +33,7 @@ public class StackOps extends Activity {
 	TextView statusField;
 	SharedPreferences settings;
 	SharedPreferences.Editor editor;
+	Boolean loggedIn;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,22 @@ public class StackOps extends Activity {
 		settings = getPreferences(MODE_PRIVATE);
 		editor = settings.edit();
 		userField.setText(settings.getString("username", ""));
+		passField.setText(settings.getString("password", ""));
+		
+		loggedIn = false;
+		
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		LocationListener locationListener = new StackLocationListener();
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 1, locationListener);
+		
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		if (settings.contains("username") && settings.contains("password")) {
+		//	doLogin(null);
+		}
 	}
 
 	@Override
@@ -57,6 +78,7 @@ public class StackOps extends Activity {
 		String password = passField.getText().toString();
 		
 		editor.putString("username", username);
+		editor.putString("password", password);
 		editor.apply();
 		
 		new LoginTask().execute(username, password);
@@ -67,7 +89,7 @@ public class StackOps extends Activity {
 
 		  @Override
 		  protected String doInBackground(String... params) {
-			  publishProgress("Fetching CSRF token");
+			  publishProgress("Fetching token");
 			  String username = params[0];
 			  String password = params[1];
 			  try {
@@ -75,22 +97,22 @@ public class StackOps extends Activity {
 				  CookieHandler.setDefault(cookieManager);
 				  
 				  // fetch CSRF token
-				  URL csrfurl = new URL("http://ops.stackunderflow.com/csrf");
+				  URL csrfurl = new URL("https://ops.stackunderflow.com/csrf");
 				  HttpURLConnection urlConnection = (HttpURLConnection) csrfurl.openConnection();		  
 				  BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 				  String token = br.readLine();
-				  publishProgress("csrf token: " + token);
 				  editor.putString("csrftoken", token);
 				  editor.apply();
 				  
 				  // now do the login
-				  publishProgress("doing login");
-				  URL loginurl = new URL("http://ops.stackunderflow.com/login");
+				  publishProgress("Sending login");
+				  URL loginurl = new URL("https://ops.stackunderflow.com/login");
 				  HttpURLConnection urlConnection2 = (HttpURLConnection) loginurl.openConnection();
 				  urlConnection2.setRequestMethod("POST");
 				  urlConnection2.setDoOutput(true);
 				  urlConnection2.setDoInput(true);
 				  urlConnection2.setRequestProperty("X-CSRFToken", token);
+				  urlConnection2.setRequestProperty("Referer", "https://ops.stackunderflow.com/login");
 				  urlConnection2.setInstanceFollowRedirects(false);
 				  OutputStream os = urlConnection2.getOutputStream();
 				  BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os));
@@ -100,13 +122,19 @@ public class StackOps extends Activity {
 				  writer.flush();
 				  writer.close();
 				  int responseCode = urlConnection2.getResponseCode();
-				  publishProgress("response code is " + responseCode);
 				  BufferedReader br2 = new BufferedReader(new InputStreamReader(urlConnection2.getInputStream()));
 				  String line;
 				  while ((line = br2.readLine()) != null) {
 				    System.out.println(line);
 				  }
-				  publishProgress("done");
+				  
+				  if (responseCode == 302) {
+					  loggedIn = true;
+					  publishProgress("Logged in successfully");
+				  } else {
+					  loggedIn = false;
+					  publishProgress("Login failed, check user and password");
+				  }
 				  
 				  
 			  } catch (Exception e) {
@@ -133,8 +161,116 @@ public class StackOps extends Activity {
 		  @Override
 		  protected void onProgressUpdate(String... text) {
 			  statusField.append(text[0] + "\n");
+			  final int scrollAmount = statusField.getLayout().getLineTop(statusField.getLineCount()) - statusField.getHeight();
+			    if (scrollAmount > 0)
+			        statusField.scrollTo(0, scrollAmount);
+			    else
+			        statusField.scrollTo(0, 0);
 			  System.out.println(text[0]);
 		  }
+	}
+	
+	private class UpdateLocationTask extends AsyncTask<Location, String, Location> {
+
+		  @Override
+		  protected Location doInBackground(Location... params) {
+			  Location loc = params[0];
+			  try {
+				  // now do the login
+				  publishProgress("updating location");
+				  URL updateurl = new URL("https://ops.stackunderflow.com/update");
+				  HttpURLConnection urlConnection2 = (HttpURLConnection) updateurl.openConnection();
+				  urlConnection2.setRequestMethod("POST");
+				  urlConnection2.setDoOutput(true);
+				  urlConnection2.setDoInput(true);
+				  String token = settings.getString("csrftoken", "");
+				  urlConnection2.setRequestProperty("X-CSRFToken", token);
+				  urlConnection2.setRequestProperty("Referer", "https://ops.stackunderflow.com/");
+				  urlConnection2.setInstanceFollowRedirects(false);
+				  OutputStream os = urlConnection2.getOutputStream();
+				  BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os));
+				  writer.write("lat="+URLEncoder.encode("" + loc.getLatitude(), "UTF-8")+
+						  	   "&lon="+URLEncoder.encode("" + loc.getLongitude(), "UTF-8")+
+						  	   "&csrf_token="+URLEncoder.encode(token, "UTF-8"));
+				  writer.flush();
+				  writer.close();
+				  int responseCode = urlConnection2.getResponseCode();
+				  InputStream in;
+				  if(responseCode != 200) {
+					    in = urlConnection2.getErrorStream();
+					    publishProgress("Failed to update position: " + responseCode);
+				  } else {
+					    in = urlConnection2.getInputStream();
+					    publishProgress("Successfully updated position");
+				  }
+				  BufferedReader br2 = new BufferedReader(new InputStreamReader(in));
+				  String line;
+				  while ((line = br2.readLine()) != null) {
+				    System.out.println(line);
+				  }				  
+				  
+			  } catch (Exception e) {
+				  e.printStackTrace();
+				  publishProgress("Exception updating pos");
+			  }
+		   return loc;
+		  }
+
+		  @Override
+		  protected void onPostExecute(Location loc) {
+		   // execution of result of Long time consuming operation
+		   //finalResult.setText(result);
+				Toast.makeText(
+		                getBaseContext(),
+		                "Location updated: Lat: " + loc.getLatitude() + " Lng: "
+		                    + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+			  
+		  }
+
+
+		  @Override
+		  protected void onPreExecute() {
+		   // Things to be done before execution of long running operation. For
+		   // example showing ProgessDialog
+		  }
+
+		 
+		  @Override
+		  protected void onProgressUpdate(String... text) {
+			  statusField.append(text[0] + "\n");
+			  System.out.println(text[0]);
+		  }
+	}
+	
+	private class StackLocationListener implements LocationListener {
+
+		@Override
+		public void onLocationChanged(Location loc) {
+			if (loggedIn) {
+				new UpdateLocationTask().execute(loc);
+			}
+			
+		}
+
+		@Override
+		public void onProviderDisabled(String arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onProviderEnabled(String arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	
 	}
 	
 }
