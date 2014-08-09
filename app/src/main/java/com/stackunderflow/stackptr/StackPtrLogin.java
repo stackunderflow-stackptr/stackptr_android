6c3619873092845feb74ec56d528e2f0a10b9c43
Michael Farrell <micolous+git@gmail.com>
2014-08-09 23:09:20 +1000

    Remove settings button on login form

--- app/src/main/java/com/stackunderflow/stackptr/StackPtrLogin.java
+++ app/src/main/java/com/stackunderflow/stackptr/StackPtrLogin.java
@@ -95,29 +95,6 @@ public class StackPtrLogin extends Activity {
 		editor.apply();
     }
 
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.stack_ptr_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
     private class ApiGetTask extends AsyncTask<String, String, String> {
 
         @Override
