8dfd4b8966271a84f4849c42d902ff526604672b
Michael Farrell <micolous@gmail.com>
2014-08-30 05:37:51 +1000

    I herd u leik documentation.

--- app/src/main/java/com/stackunderflow/stackptr/StackPtrUtils.java
+++ app/src/main/java/com/stackunderflow/stackptr/StackPtrUtils.java
@@ -8,6 +8,15 @@ import android.content.res.Resources;
  * Based on stackops-utils.js
  */
 public class StackPtrUtils {
+    /**
+     * Returns a human-friendly distance value.  This only supports metric values, and is always
+     * shown in short form.  a11y applications interpret metric suffixes correctly, so for the
+     * value "100 km", it will say "one hundred kilometres".
+     *
+     * This value is i18n-aware.
+     * @param distance A distance value, in metres.
+     * @return
+     */
     public static String distanceFormat(double distance) {
         if (distance > 100000) {
             return String.format("%.0f km", distance/1000);
@@ -18,6 +27,15 @@ public class StackPtrUtils {
         }
     }
 
+    /**
+     * Returns the amount of time ago a given UNIX timestamp was.  Timestamps in the future are not
+     * supported.  This function is i18n-aware.
+     * @param time The UNIX time to convert.
+     * @param long_form If true, this will convert the value in a non-abbreviated form, appropriate
+     *                  for use with a11y applications like Talkback.
+     * @param ctx Context to use for i18n
+     * @return
+     */
     public static String timeFormat(long time, boolean long_form, Context ctx) {
         time = (System.currentTimeMillis() / (long)1000) - time;
         Resources res = ctx.getResources();
@@ -41,9 +59,17 @@ public class StackPtrUtils {
         }
     }
 
+    /**
+     * Gets the "short" (abbreviated) compass direction for the given heading.  This method is
+     * i18n-aware.
+     *
+     * @param heading The heading to convert, in decimal degrees.
+     * @param ctx Context to use for i18n
+     * @return The translated string appropriate for that heading.
+     */
     public static String getShortCompassName(double heading, Context ctx) {
         // Sometimes we get back a -180 .. 180 value if it's a big distance?
-        // TODO: Check the documentation to see if this is mentioned.
+        // TODO: Check the documentation to see if this is mentioned. (a.l.Location.bearingTo)
         heading %= 360;
 
         if (heading < 22.5) {
@@ -67,7 +93,19 @@ public class StackPtrUtils {
         }
     }
 
+    /**
+     * Gets the "long" (full) compass direction for the given heading.  This method is i18n-aware.
+     * This value is used for giving descriptive text of the current heading, used by Talkback for
+     * a11y.
+     * @param heading The heading to convert, in decimal degrees.
+     * @param ctx Context to use for i18n
+     * @return The translated string appropriate for that heading.
+     */
     public static String getLongCompassName(double heading, Context ctx) {
+        // Sometimes we get back a -180 .. 180 value if it's a big distance?
+        // TODO: Check the documentation to see if this is mentioned. (a.l.Location.bearingTo)
+        heading %= 360;
+
         if (heading < 22.5) {
             return ctx.getString(R.string.compass_long_N);
         } else if (heading < 67.5) {
