0b1f04589cf2fedead19c6344b33f947dbb526c5
Geordie Millar <gm@stackunderflow.com>
2015-09-06 22:08:01 +1000

    Fixes for new format

--- app/src/main/java/com/stackunderflow/stackptr/StackPtr.java
+++ app/src/main/java/com/stackunderflow/stackptr/StackPtr.java
@@ -50,7 +50,7 @@ public class StackPtr extends Activity {
     Location lastloc;
 
     UserArrayAdapter adapter;
-    JSONObject jUsers;
+    JSONArray jUsers;
     ArrayList<Integer> tUsers;
     JSONObject jMe;
 
@@ -75,25 +75,6 @@ public class StackPtr extends Activity {
         adapter = new UserArrayAdapter(this);
         listview.setAdapter(adapter);
 
-        /*listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, final View view,
-                                    int position, long id) {
-                final String item = (String) parent.getItemAtPosition(position);
-                view.animate().setDuration(2000).alpha(0)
-                        .withEndAction(new Runnable() {
-                            @Override
-                            public void run() {
-                                list.remove(item);
-                                adapter.notifyDataSetChanged();
-                                view.setAlpha(1);
-                            }
-                        }
-                 );
-            }
-
-        });*/
 	}
 
 	@Override
@@ -104,19 +85,16 @@ public class StackPtr extends Activity {
     @Override
     public void onResume() {
         super.onResume();
-        //Toast.makeText(getBaseContext(), "onResume", Toast.LENGTH_SHORT).show();
-        //statusField.setText("Waiting for GPS...\n");
+
         fglm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         fgll = new StackPtrFGListener();
         fglm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0.0f, fgll);
-
     }
 
     @Override
     public void onPause() {
         super.onPause();
         fglm.removeUpdates(fgll);
-        //Toast.makeText(getBaseContext(), "onPause", Toast.LENGTH_SHORT).show();
     }
 
 	@Override
@@ -153,33 +131,6 @@ public class StackPtr extends Activity {
     private class ApiGetUsers extends AsyncTask<Void, String, String> {
         @Override
         protected String doInBackground(Void... params) {
-            /*
-            ///// DEBUG DEBUG DEBUG
-            Location l1 = new Location("StackTest");
-            l1.setLatitude(35);
-            l1.setLongitude(-140);
-            Location l2 = new Location("StackTest");
-            l2.setLatitude(-35);
-            l2.setLongitude(140);
-            Location l3 = new Location("StackTest");
-            l3.setLatitude(-32);
-            l3.setLongitude(143);
-
-
-            System.out.println("l1->l2 bearing: " + l1.bearingTo(l2));     // -124.18
-            System.out.println("l1->l2 distance: " + l1.distanceTo(l2));   // 1.13E7
-
-            System.out.println("l2->l1 bearing: " + l2.bearingTo(l1));     // 55.81
-            System.out.println("l2->l1 distance: " + l2.distanceTo(l1));   // 1.13E7
-
-
-            System.out.println("l2->l3 bearing: " + l2.bearingTo(l3));     // 40.79
-            System.out.println("l2->l3 distance: " + l2.distanceTo(l3));   // 434028.9
-
-            System.out.println("l1->l3 bearing: " + l1.bearingTo(l3));     // -123.16089
-            System.out.println("l1->l3 distance: " + l1.distanceTo(l3));   // 1.09E7
-            ///// DEBUG DEBUG DEBUG
-            // */
 
             OkUrlFactory urlFactory = new OkUrlFactory(new OkHttpClient());
 
@@ -193,24 +144,17 @@ public class StackPtr extends Activity {
             }
 
             try {
-                // fetch user list token
-
                 URL userurl = new URL(serverHost + "/users?apikey=" + apikey);
                 HttpURLConnection userConnection = urlFactory.open(userurl);
-                //(HttpURLConnection) userurl.openConnection();
-                //BufferedReader br = new BufferedReader(new InputStreamReader(userConnection.getInputStream()));
-                //String token = br.readLine();
 
                 int responseCode = userConnection.getResponseCode();
                 if(responseCode != 200) {
-                    publishProgress("Failed to update position: " + responseCode);
+                    publishProgress("Failed to update users: " + responseCode);
                     return "";
                 }
 
                 InputStream in = userConnection.getInputStream();
                 BufferedReader br2 = new BufferedReader(new InputStreamReader(in));
-
-                // todo: check for request success
 
                 StringBuilder json = new StringBuilder();
                 String line;
@@ -218,16 +162,17 @@ public class StackPtr extends Activity {
                 	json.append(line);
                 }
 
-                JSONArray jObj = new JSONArray(json.toString());
+                JSONArray jUsersResponse = new JSONArray(json.toString());
 
                 jUsers = null;
                 jMe = null;
 
-                for (int i=0; i<jObj.length(); i++) {
-                    JSONObject msg = jObj.getJSONObject(i);
+                // fixme: more than one?
+                for (int i=0; i<jUsersResponse.length(); i++) {
+                    JSONObject msg = jUsersResponse.getJSONObject(i);
                     String name = msg.getString("type");
                     if (name.equals("user")) {
-                        jUsers = msg.getJSONObject("data");
+                        jUsers = msg.getJSONArray("data");
                     } else if (name.equals("user-me")) {
                         jMe = msg.getJSONObject("data");
                     }
@@ -242,18 +187,6 @@ public class StackPtr extends Activity {
                 }
 
 
-                //return "";
-
-                //jUsers = jObj.getJSONArray("following");
-
-                //if (jObj.isNull("me")) {
-                    //publishProgress(getString(R.string.my_location_is_unknown));
-                //    return "My location is unknown to server";
-                //}
-                //jMe = jObj.getJSONObject("me");
-
-                //StringBuilder res = new StringBuilder();
-                //tUsers = new ArrayList<String>();
                 tUsers.clear();
 
                 if (lastloc == null) {
@@ -272,38 +205,15 @@ public class StackPtr extends Activity {
                 }
 
 
-
-
-                Iterator<String> userIterator = jUsers.keys();
-                while (userIterator.hasNext()) {
-                    JSONObject thisUser = jUsers.getJSONObject(userIterator.next());
+                for (int i=0; i<jUsers.length(); i++) {
+                    JSONObject thisUser = jUsers.getJSONObject(i);
                     Integer user = thisUser.getInt("id");
                     tUsers.add(user);
-                    /*JSONArray loc_s = thisUser.getJSONArray("loc");
-                    double lat = loc_s.getDouble(0);
-                    double lon = loc_s.getDouble(1);
-                    int lastupd = thisUser.getInt("lastupd");
-
-                    Location userLocation = new Location("StackPtr");
-                    userLocation.setLatitude(lat);
-                    userLocation.setLongitude(lon);
-
-                    float dist = lastloc.distanceTo(userLocation);
-                    float bearing = lastloc.bearingTo(userLocation);
-
-                    //String prog = String.format("%s %.2f %.0f %d\n",user,dist,bearing,lastupd);
-                    //String prog = user + " " + dist + "m " + bearing + " deg " + lastupd + "s ago\n";
-
-                    //String prog = "user: " + user + " lat: " + lat + " lon: " + lon + " lastupd " + lastupd + "\n";
-                    String prog = user + " " + StackPtrUtils.distanceFormat(dist)
-                                       + " " + StackPtrUtils.headingFormat(bearing)
-                                       + " " + StackPtrUtils.timeFormat(lastupd) + "\n";
-                    res.append(prog);*/
                 }
 
                 br2.close();
                 userConnection.disconnect();
-                return "";//res.toString();
+                return "";
             } catch (Exception e) {
                 e.printStackTrace();
                 return "Error fetching list.";
@@ -313,8 +223,6 @@ public class StackPtr extends Activity {
         @Override
         protected void onPostExecute(String result) {
             adapter.notifyDataSetChanged();
-            //System.out.printf("notifyDataSetChanged()\n");
-            //statusField.setText(result + "\n");
         }
 
         @Override
@@ -325,9 +233,6 @@ public class StackPtr extends Activity {
         @Override
         protected void onProgressUpdate(String... text) {
             System.out.printf("%s\n", (Object) text);
-            //for (String p: text) {
-            //    statusField.append(p + "\n");
-            //}
         }
 
     }
@@ -363,16 +268,18 @@ public class StackPtr extends Activity {
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(int position, View rowView, ViewGroup parent) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View rowView = inflater.inflate(R.layout.userview, parent, false);
+            if (rowView == null) {
+                rowView = inflater.inflate(R.layout.userview, parent, false);
+            }
             TextView firstLine = (TextView) rowView.findViewById(R.id.firstLine);
             TextView secondLine = (TextView) rowView.findViewById(R.id.secondLine);
             ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
             //ImageButton mapButton = (ImageButton) rowView.findViewById(R.id.mapButton);
 
             try {
-                JSONObject jUser = jUsers.getJSONObject(Integer.toString(tUsers.get(position))); // FIXME
+                JSONObject jUser = jUsers.getJSONObject(position);
                 final String username = jUser.getString("username");
                 firstLine.setText(username);
 
