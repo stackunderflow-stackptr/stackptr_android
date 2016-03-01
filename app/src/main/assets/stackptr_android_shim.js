d67b7475eb01e236a7e7be25c25a6ed97d39fbfa
Geordie Millar <gm@stackunderflow.com>
2016-03-01 22:59:07 +1100

    set a flag for stackptr_android, and load the android CSS in platform shim

--- app/src/main/assets/stackptr_android_shim.js
+++ app/src/main/assets/stackptr_android_shim.js
@@ -1,6 +1,9 @@
+stackptr_android = true;
 var host = StackPtrAndroidShim.get_host().split("//");
 stackptr_server_base_host = host[1].split("/")[0];
 stackptr_server_base_protocol = host[0];
+
+$('head').append($("<link>", {rel: 'stylesheet', type: 'text/css', href: 'static/css/stackptr_android.css'}));
 
 stackptr_apikey = StackPtrAndroidShim.get_apikey();
 
